c815223b1b3adb921ee2af16060392d497cc30ad
f2024376146-crypto <f2024376146@umt.edu.pk>
2026-01-20 12:39:04 +0500

    Cleaned main.cppand added final test cases

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-#include "LinkedList.h"
-#include "ParkingSlot.h"
+#include "Zone.h"
 
 int main() {
-
-    LinkedList<ParkingSlot> system;
-
-
-    system.add(ParkingSlot(101, 1));
-    system.add(ParkingSlot(102, 1));
-
-    std::cout << "Smart Parking System Initialized!" << std::endl;
     
+    Zone myZone(1, "VIP-Parking");
 
-    if (system.head != nullptr) {
-        std::cout << "Latest Slot added: " << system.head->data.slotID << std::endl;
-    }
+
+    myZone.addSlot(101);
+    myZone.addSlot(102);
+    myZone.addSlot(103);
+
+    myZone.displayZoneStatus();
 
     return 0;
 }
